6cb3ea535c6da22461f6bb03564b6bfef0a651b1
Sergii Ponomarov <sergii.ponomarov@gmail.com>
2012-12-25 22:32:17 +0200

    Add test for Socket.bind

--- your_first_socket_test.rb
+++ your_first_socket_test.rb
@@ -8,4 +8,14 @@ class YourFirstSocketTest < MiniTest::Unit::TestCase
 
     assert_match /#<Socket:fd \d>/, socket.inspect
   end
+
+  def test_socket_bind
+    socket = Socket.new(:INET, :STREAM)
+    addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+    socket.bind(addr)
+
+    assert_raises Errno::EADDRINUSE do
+      Socket.new(:INET, :STREAM).bind(addr)
+    end
+  end
 end
