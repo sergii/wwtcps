eabd8058d9b88eebf8b469e952dc27d8a66d6b96
Sergii Ponomarov <sergii.ponomarov@gmail.com>
2013-01-09 23:35:25 +0200

    Add test Socket Accept. Use Threads

--- chapter_3_socket_test.rb
+++ chapter_3_socket_test.rb
@@ -18,8 +18,23 @@ class Chapter3SocketTest < MiniTest::Unit::TestCase
   	addr = Socket.pack_sockaddr_in(4482, '0.0.0.0')
   	socket.bind(addr)
   	socket.listen(5)
-
     # Nothing came up
+  end
 
+  def test_socket_accept
+    socket = Socket.new(:INET, :STREAM)
+    addr = Socket.pack_sockaddr_in(4483, '0.0.0.0')
+    socket.bind(addr)
+    socket.listen(5)
+
+    Thread.new do
+      system("echo ohai | nc localhost 4483")
+    end
+
+    connection, _ = socket.accept
+    c_class = connection.class
+    connection.shutdown
+
+    assert_equal Socket, c_class
   end
 end
